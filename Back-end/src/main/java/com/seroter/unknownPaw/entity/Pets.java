d5fe148b5e64599be953e2e56dfed62f6a3cc08a
BaeWonyoung <100410wy@gmail.com>
2025-04-02 17:00:57 +0900

    note added

--- Back-end/src/main/java/com/seroter/unknownPaw/entity/Pets.java
+++ Back-end/src/main/java/com/seroter/unknownPaw/entity/Pets.java
@@ -1,4 +1,44 @@
 package com.seroter.unknownPaw.entity;
 
-public class Pets {
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.mapping.UniqueKey;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Table(name = "m_members")
+public class Pets extends BaseEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long petId;
+
+  private String petName;
+  private String breed;
+  private int petBirth;
+  private boolean petGender;
+  private double weight;
+  private String petMbti; // 강아지 성격
+  private boolean neutering;
+  private String petIntroduce;
+
+
+  private LocalDateTime regDate;
+  private LocalDateTime modDate;
+
+
+  @ManyToOne
+  private Members members;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  private Long imgId;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  private Long petOwnerId;
+
 }
