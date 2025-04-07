c22591cade7b94162070d160b9259e15d87e7b41
NuWahm <matsari0194@gmail.com>
2025-04-07 22:57:28 +0900

    MemberRequestDto & MemberResponseDto deleted

--- Back-end/src/main/java/com/seroter/unknownPaw/dto/MemberResponseDto.java
+++ Back-end/src/main/java/com/seroter/unknownPaw/dto/MemberResponseDto.java
@@ -1,4 +1,4 @@
-package com.seroter.unknownPaw.dto.member;
+package com.seroter.unknownPaw.dto;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -7,14 +7,15 @@ import java.time.LocalDateTime;
 
 @Getter
 @Builder
-public class MemberResponseDto {
-    private Long id;                   // 회원 ID
-    private String email;             // 이메일
-    private String nickname;          // 닉네임
-    private String profileImagePath;  // 프로필 이미지
-    private float pawRate;            // 평점
-    private boolean emailVerified;    // 이메일 인증 여부
-    private String role;              // 역할
-    private String status;            // 상태
-    private LocalDateTime regDate;    // 가입일
+public class MemberResponseDTO {
+  private Long mid;                   // 회원 ID
+  private String email;             // 이메일
+  private String nickname;          // 닉네임
+  private String profileImagePath;  // 프로필 이미지
+  private float pawRate;            // 평점
+  private boolean emailVerified;    // 이메일 인증 여부
+  private String role;              // 역할
+  private String status;            // 상태
+  private LocalDateTime regDate;    // 가입일
 }
+
