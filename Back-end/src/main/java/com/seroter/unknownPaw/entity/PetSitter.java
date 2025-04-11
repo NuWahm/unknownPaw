629bb416055e7c0287e3a56d90a8d3ee235efda7
kjh0517 <cavin56@naver.com>
2025-04-11 16:05:30 +0900

    owner,sitter service updated

--- Back-end/src/main/java/com/seroter/unknownPaw/entity/PetSitter.java
+++ Back-end/src/main/java/com/seroter/unknownPaw/entity/PetSitter.java
@@ -1,9 +1,9 @@
 package com.seroter.unknownPaw.entity;
 
-import com.seroter.unknownPaw.entity.Member;
-import com.seroter.unknownPaw.entity.Post;
-import com.seroter.unknownPaw.entity.ServiceCategory;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
 
 @Entity
